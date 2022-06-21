1e9e64e0878805c9d8c586a6dab36ca1063a027f
Kazuto <felipe381xbox@gmail.com>
2022-06-21 05:05:45 -0400

    Primera pagina funcional de lista de Comics.

--- comiczoom/Controllers/ComicsController.cs
+++ comiczoom/Controllers/ComicsController.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using prueba.Models;
 
 namespace prueba.Controllers
 {
     [Authorize]
     public class ComicsController : Controller
     {
+        Comic com = new Comic();
         // GET: Comics
-        public ActionResult ComicsList()
+        [Permissions.PermissionsRol(Rol.Administrador)]
+        public ActionResult ComicsList(FormCollection formCollection)
         {
+            string _categoria = formCollection["Categoria"];
+            string _estado = formCollection["Estado"];
+           
 
+            ViewBag.Comics = com.ListarComics(_estado, _categoria);
+            ViewBag.ComboCategoria = com.ComboCategoria();
+            ViewBag.ComboEstado = com.ComboEstado();
+            ViewBag.ErrorInsertComic = "d-none";
             return View();
         }
     }
