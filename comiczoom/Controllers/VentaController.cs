8c4ae0c68affbfeb0cf7108ad19cb111aae76dc5
Abraham Sánchez <76190586+asanchez1415@users.noreply.github.com>
2022-07-01 00:14:37 -0500

    Respaldo 1

---
+++ comiczoom/Controllers/VentaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prueba.Controllers
+{
+    public class VentaController : Controller
+    {
+        // GET: Venta
+        public ActionResult ListaVentas()
+        {
+
+            return View();
+        }
+    }
+}
