f5681768b99906f0c997d77f92a5ce8e39baf80a
FushiKami <diego.github13@gmail.com>
2022-07-01 02:03:29 -0400

    Stock Comics hecho e index actualizado
    a.a

---
+++ comiczoom/Controllers/StockComicsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prueba.Models;
+
+namespace prueba.Controllers
+{
+    public class StockComicsController : Controller
+    {
+        StockComics sc = new StockComics();
+        Comic com = new Comic();
+        Branches bra = new Branches();
+        // GET: StockComics
+        public ActionResult StockComList(FormCollection formCollection)
+        {
+            string _comic = formCollection["Comics"];
+
+            string _sucursal = formCollection["Sucursal"];
+
+            ViewBag.NombreUsuario = Session["Nombres"];
+
+            ViewBag.ListCom = sc.ListarStockComics(_comic, _sucursal);
+
+            ViewBag.ComboCom = com.ComboComic();
+
+            ViewBag.ComboSuc = bra.ComboSucursal();
+
+            return View();
+        }
+    }
+}
---
+++ comiczoom/Models/StockComics.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace prueba.Models
+{
+    public class StockComics
+    {
+        public int Contador { get; set; }
+        //
+        public int idCOM { get; set; }
+        public int idSUC { get; set; }
+        //
+        public int Cantidad { get; set; }
+        public string NombreComic { get; set; }
+        public string NombreSucursal { get; set; }
+
+        public DateTime Fecha { get; set; }
+        public int TipoTransacción { get; set; }
+
+        private List<StockComics> ListCOM { get; set; } = new List<StockComics>();
+
+        public List<StockComics> ListarStockComics(string pNomCom, string pNomSuc)
+        {
+            ListCOM = new List<StockComics>();
+            ConnectionDB connection = new ConnectionDB();
+            SqlDataReader registros = null;
+            connection.Open();
+
+            SqlCommand querySel = new SqlCommand($@"SELECT C.id idCOM, C.nombre nombreC, S.id idSUC, S.nombre nombreS
+                                FROM COMIC C, SUCURSAL S
+								WHERE C.nombre LIKE '%{pNomCom}%'
+								AND S.nombre LIKE '%{pNomSuc}%';", connection.connectDb);
+
+            registros = querySel.ExecuteReader();
+
+            int count = 1;
+
+            while (registros.Read())
+            {
+                var registro = new StockComics()
+                {
+                    Contador = count,
+                    idCOM = Convert.ToInt32(registros["idCOM"]),
+                    NombreComic = registros["nombreC"].ToString(),
+                    idSUC = Convert.ToInt32(registros["idSUC"]),
+                    NombreSucursal = registros["nombreS"].ToString()
+                };
+                ListCOM.Add(registro);
+                count++;
+            }
+            connection.Close();
+
+            return ListCOM;
+        }
+        public int CalcularStock(int pCOM, int pSUC)
+        {
+            ListCOM = new List<StockComics>();
+            ConnectionDB connection = new ConnectionDB();
+            SqlDataReader registros = null;
+            connection.Open();
+
+            SqlCommand querySel = new SqlCommand($@"SELECT idCOM, idSUC, fecha, hora,
+                        SUM(cantidad * case when tipoTransaccion = 0 then 1 else -1 END) as movimientos 
+                        FROM STOCK_COMIC
+                        WHERE idCOM = {pCOM} AND idSUC = {pSUC}
+                        GROUP BY idCOM, idSUC, fecha, hora", connection.connectDb);
+
+            registros = querySel.ExecuteReader();
+
+            while (registros.Read())
+            {
+                var registro = new StockComics()
+                {
+                    Cantidad = Convert.ToInt32(registros["movimientos"]),
+                };
+                ListCOM.Add(registro);
+            }
+            connection.Close();
+
+            int stock = 0;
+            for (int i = 0; i < ListCOM.Count; i++)
+            {
+                stock += ListCOM[i].Cantidad;
+            }
+
+            return stock;
+
+        }
+    }
+}
